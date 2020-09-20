b5b0821fd77f5325f7a9c45818a9e0b671496cbc
Fernanda Dias Silva <fdsferna@gmail.com>
2020-09-20 20:39:54 -0300

    Projeto Proffy NLW 2 Discovery Finalizado

---
+++ src/database/fakedatabase.js
@@ -0,0 +1,25 @@
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "41989898987",
+        bio: "Entusiasta das melhores tecnologias de química avançada Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Fernanda Dias",
+        avatar: "https://lh3.googleusercontent.com/ogw/ADGmqu_bW9SnQfHiPm-z_KxkxwWRWPNpUmoIbMBskkAL=s83-c-mo",
+        whatsapp: "43996870552",
+        bio: "Entusiasta das melhores tecnologias de química avançada Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+
+    }
+]
